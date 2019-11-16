6161be79041a8a9c3b52ffc71273108cdf3d4ba8
Haritima23 <haritima@udel.edu>
2019-11-16 18:16:32 -0500

    Coins and Score Added

--- Source Code.py
+++ Source Code.py
@@ -1,5 +1,5 @@
 import arcade
-
+import random
 """ RESOURCES
     arcade.academy
     wikipedia
@@ -16,20 +16,21 @@ class X_COSMOS(arcade.Window):
         super().__init__(SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_TITLE)
         self.time=0
         self.score=0
-        self.level =0
-        self.lives=0
-        self.player_list=None
+        self.level =1
+        self.lives=2
+        self.player_list=self.coin_list=None
         self.background_list=None
         self.spike_list=None
+        self.collision_list=None
         self.physics_engine=None
 
     def setup(self):
         arcade.set_background_color(arcade.color.CORNFLOWER_BLUE)
 
         self.player_list=arcade.SpriteList()
-        self.player_sprite=arcade.Sprite("images/character2.png",1.5)
+        self.player_sprite=arcade.Sprite("images/character2.png",1)
         self.player_sprite.center_x=50
-        self.player_sprite.center_y =270
+        self.player_sprite.center_y =250
         self.player_list.append(self.player_sprite)
 
         self.background_list=arcade.SpriteList()
@@ -42,9 +43,18 @@ class X_COSMOS(arcade.Window):
         for i in range(1,3):
             self.spike_sprite = arcade.Sprite("images/spikes.png",0.8)
             self.spike_sprite.center_x=200+5*i*spike_distance
-            self.spike_sprite.center_y=270
+            self.spike_sprite.center_y=240
             self.spike_list.append(self.spike_sprite)
-        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite,self.spike_list)
+
+
+        self.coin_list=arcade.SpriteList()
+        for i in range(0,5):
+            self.coin_sprite=arcade.Sprite("images/Coin.png",0.6)
+            self.coin_sprite.center_x=50*i+600
+            self.coin_sprite.center_y=200
+            self.coin_list.append(self.coin_sprite)
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.spike_list)
+
 
     def on_draw(self):
         arcade.start_render()
@@ -52,6 +62,7 @@ class X_COSMOS(arcade.Window):
         self.background_list.draw()
         self.player_list.draw()
         self.spike_list.draw()
+        self.coin_list.draw()
 
         arcade.draw_text(str("SCORE:"), 30, 90, arcade.color.BLACK, 40)
         arcade.draw_text(str(self.score), 200, 90, arcade.color.RED, 40)
@@ -82,6 +93,11 @@ class X_COSMOS(arcade.Window):
     def on_update(self,delta_time):
         self.physics_engine.update()
         self.time+=(round(delta_time)+1)
+        self.collision_list=arcade.check_for_collision_with_list(self.player_sprite,self.coin_list)
+        for coin in self.collision_list:
+            coin.remove_from_sprite_lists()
+            self.score+=10
+
 
 
 
